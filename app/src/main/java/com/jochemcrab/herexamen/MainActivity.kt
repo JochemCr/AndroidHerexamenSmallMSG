3b1eb3cab31cbe528f25330bc62110ed5f836b0b
Jochem Crab <jochemcrab14@gmail.com>
2022-07-25 17:43:32 +0200

    Set base for recycler

--- app/src/main/java/com/jochemcrab/herexamen/MainActivity.kt
+++ app/src/main/java/com/jochemcrab/herexamen/MainActivity.kt
@@ -2,10 +2,17 @@ package com.jochemcrab.herexamen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun sendMessage(view: View){
+        val sentMessage = findViewById<EditText>(R.id.editTextInput)
+        findViewById<TextView>(R.id.display_message).text = sentMessage.text.toString()
     }
 }
